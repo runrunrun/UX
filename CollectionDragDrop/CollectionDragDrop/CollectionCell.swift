cfd3e4bd3777af1d4a509fc3ccf4494aaf4d419c
Hari Kunwar <hkunwar>
2017-01-26 07:21:03 -0800

    CollectionDragDrop: Added support for long press gesture.

--- CollectionDragDrop/CollectionDragDrop/CollectionCell.swift
+++ CollectionDragDrop/CollectionDragDrop/CollectionCell.swift
@@ -8,22 +8,55 @@
 
 import UIKit
 
+protocol CollectionCellDelegate: class {
+    func longPressedCollectionCell(cell: CollectionCell)
+}
+
 class CollectionCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
-
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var containerViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var containerViewHeight: NSLayoutConstraint!
+    
+    let containerPadding: CGFloat = 20
+    
+    private var isActive = false
+    private var longPress: UILongPressGestureRecognizer?
+    
+    weak var delegate: CollectionCellDelegate?
+    
     override func didMoveToWindow() {
         super.didMoveToWindow()
-    
-        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(editMode(_:)))
-        self.addGestureRecognizer(longPress)
+                
+        if longPress == nil {
+            longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressAction(gesture:)))
+            self.addGestureRecognizer(longPress!)
+        }
     }
     
-    func editMode(_ active: Bool) {
-        if active {
+    var isEditing: Bool = false {
+        didSet {
             
+            guard containerViewWidth.constant > 0 else {
+                print("Constraints not set yet")
+                return
+            }
+            
+            if isEditing {
+                containerViewWidth.constant -= containerPadding
+                containerViewHeight.constant -= containerPadding
+            }
+            else {
+                containerViewWidth.constant += containerPadding
+                containerViewHeight.constant += containerPadding
+            }
         }
-        else {
-        
+    }
+
+    func longPressAction(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            delegate?.longPressedCollectionCell(cell: self)
         }
     }
     
--- CollectionDragDrop/CollectionDragDrop/ViewController.swift
+++ CollectionDragDrop/CollectionDragDrop/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    fileprivate var isEditingCells: Bool?
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +19,11 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
@@ -29,8 +31,28 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionCell
         cell.titleLabel.text = "Cell Number \(indexPath.row)"
+        cell.delegate = self
+        if let editing = isEditingCells {
+            cell.isEditing = editing
+        }
+        
         return cell
+    }
+}
+
+extension ViewController: CollectionCellDelegate {
+    func longPressedCollectionCell(cell: CollectionCell) {
+        if let editing = isEditingCells {
+            isEditingCells = !editing
+        }
+        else {
+            isEditingCells = true
+        }
+        
+        collectionView.reloadData()
     }
     
 }
+
+
 
