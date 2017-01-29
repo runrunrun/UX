a411685fcf483b5f92964dd7c143f8fb8a49d770
Hari Kunwar <hkunwar>
2017-01-29 05:52:03 -0800

    CollectionDragDrop: adding selection animation

--- CollectionDragDrop/CollectionDragDrop/CollectionCell.swift
+++ CollectionDragDrop/CollectionDragDrop/CollectionCell.swift
@@ -24,11 +24,14 @@ class CollectionCell: UICollectionViewCell {
     private var isActive = false
     private var longPress: UILongPressGestureRecognizer?
     
+    private var scaleUp: CABasicAnimation?
+    private var vibrate: CABasicAnimation?
+    
     weak var delegate: CollectionCellDelegate?
     
     override func didMoveToWindow() {
         super.didMoveToWindow()
-                
+        
         if longPress == nil {
             longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressAction(gesture:)))
             self.addGestureRecognizer(longPress!)
@@ -38,25 +41,51 @@ class CollectionCell: UICollectionViewCell {
     var isEditing: Bool = false {
         didSet {
             
-            guard containerViewWidth.constant > 0 else {
-                print("Constraints not set yet")
-                return
-            }
-            
             if isEditing {
-                containerViewWidth.constant -= containerPadding
-                containerViewHeight.constant -= containerPadding
+                // Vibrate with cancel button
             }
             else {
-                containerViewWidth.constant += containerPadding
-                containerViewHeight.constant += containerPadding
+                // Stop vibrating and hide cancel button.
+                
             }
         }
     }
 
     func longPressAction(gesture: UILongPressGestureRecognizer) {
+        let scaleValue = 1.2
+        let scaleDuration = 0.1
         if gesture.state == .began {
-            delegate?.longPressedCollectionCell(cell: self)
+            //delegate?.longPressedCollectionCell(cell: self)
+            
+            // Scale Up
+            scaleUp = CABasicAnimation(keyPath: "transform.scale")
+            scaleUp?.fromValue = 1.0
+            scaleUp?.toValue = scaleValue
+            scaleUp?.duration = scaleDuration
+            scaleUp?.fillMode = kCAFillModeForwards
+            scaleUp?.isRemovedOnCompletion = false
+            self.layer.add(scaleUp!, forKey: "scalingUp")
+        }
+        
+        if gesture.state == .ended {
+            // Scale Down
+            let scaleDown = CABasicAnimation(keyPath: "transform.scale")
+            scaleDown.fromValue = scaleValue
+            scaleDown.toValue = 1.0
+            scaleDown.duration = scaleDuration
+            self.layer.add(scaleDown, forKey: "scalingDown")
+            self.layer.removeAnimation(forKey: "scalingUp")
+            
+            // Vibrate
+            let angle = Float.pi/72 // 2.5 degree
+            vibrate = CABasicAnimation(keyPath: "transform.rotation")
+            vibrate?.beginTime = CACurrentMediaTime() + scaleDuration
+            vibrate?.duration = 0.15
+            vibrate?.fromValue = -angle
+            vibrate?.toValue = angle
+            vibrate?.autoreverses = true
+            vibrate?.repeatCount = .infinity
+            self.layer.add(vibrate!, forKey: "vibrate")
         }
     }
     
